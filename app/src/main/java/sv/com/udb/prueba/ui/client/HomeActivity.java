e2eb4fc51cf905278a468390f3419debe91aadb3
AnibalMisaki <anibalmisaki@gmial.com>
2022-04-11 22:22:12 -0600

    finale commit

---
+++ app/src/main/java/sv/com/udb/prueba/ui/client/HomeActivity.java
@@ -0,0 +1,46 @@
+package sv.com.udb.prueba.ui.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import sv.com.udb.prueba.R;
+import sv.com.udb.prueba.databinding.ActivityHomeBinding;
+import sv.com.udb.prueba.ui.client.automovil.AutoMovilActivity;
+import sv.com.udb.prueba.ui.client.favoritos.FavoritosActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+    private Integer userId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        userId = getIntent().getIntExtra("userId",-1);
+        if(userId == -1){
+            Toast.makeText(this,"Failed to retrieve user",Toast.LENGTH_LONG).show();
+            finish();
+        }
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnCarros.setOnClickListener(this::btnCarrosListener);
+        binding.btnFav.setOnClickListener(this::btnFavListener);
+    }
+
+    private void btnCarrosListener(View view){
+        Intent i = new Intent(this, AutoMovilActivity.class);
+        i.putExtra("userId",userId);
+        startActivity(i);
+    }
+
+    private void btnFavListener(View view){
+        Intent i = new Intent(this, FavoritosActivity.class);
+        i.putExtra("userId",userId);
+        startActivity(i);
+    }
+}
